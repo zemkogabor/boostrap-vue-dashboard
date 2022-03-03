cd627189f4b4df41d854562807eb60cc2d8ae6b0
Gabor Zemko <gaborzemko@gmail.com>
2022-03-04 00:55:44 +0100

    Navbar dropdown added and other sidebar fixes.

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -1,12 +1,11 @@
 <template>
   <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-    <div class="navbar-brand col-md-3 col-lg-2 me-0 px-3">
+    <div class="navbar-brand col-md-3 col-lg-2 d-none d-md-block px-3 me-auto">
       {{ brandName }}
     </div>
-
     <button
       type="button"
-      class="navbar-toggler position-absolute d-md-none collapsed"
+      class="navbar-toggler d-md-none collapsed me-auto"
       data-bs-toggle="collapse"
       data-bs-target="#sidebarMenu"
       aria-controls="sidebarMenu"
@@ -14,12 +13,32 @@
     >
       <span class="navbar-toggler-icon" />
     </button>
+    <div v-if="navbarDropdown !== null" class="dropdown">
+      <a
+          id="navbarDropdownMenuLink"
+          class="nav-link text-light"
+          href="#"
+          role="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+      >
+        <i :class="navbarDropdown.iconClass" />
+        <span v-if="navbarDropdown.title !== null" class="ms-2" v-text="navbarDropdown.title" />
+      </a>
+      <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
+        <li v-for="(dropdownItem, index) in navbarDropdown.items" :key="index">
+          <a class="dropdown-item" href="#" @click="dropdownItem.callable">
+            {{ dropdownItem.label }}
+          </a>
+        </li>
+      </ul>
+    </div>
   </nav>
 
   <div class="container-fluid">
     <div class="row">
-      <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-        <div class="position-sticky">
+      <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse collapse-horizontal">
+        <div class="position-sticky sidebar-content">
           <ul class="nav flex-column">
             <template v-for="(item, index) in items" :key="index">
               <div v-if="item.heading !== undefined" class="sidebar-heading px-3 mt-3 text-muted">
@@ -52,16 +71,26 @@
 
 <script>
 /**
- * @typedef {{
- *   ?heading: String,
- *   subItems: {
- *     label: String,
- *     ?iconClass: String,
- *     route: {
- *       name: String
- *     },
- *   }[],
- * }} Item
+ * @typedef {Object} SidebarItem
+ * @property {string|null} heading - Sidebar item heading.
+ * @property {SubItem[]} subItems - Sidebar item sub items.
+ *
+ * @typedef {Object} SubItem
+ * @property {string} label - Sub item label.
+ * @property {string} iconClass - Sub item icon class.
+ * @property {Route} route
+
+ * @typedef {Object} Route
+ * @property {string} name - Route name.
+ *
+ * @typedef {Object} NavbarDropdown
+ * @param {string|null} title - Title
+ * @param {string} iconClass - Icon class
+ * @param {NavbarDropdownItem[]} items - Dropdown items.
+ *
+ * @typedef {Object} NavbarDropdownItem
+ * @param {string} label - Label
+ * @param {function} callable - Callable funcion onclick
  */
 export default {
   name: 'Navbar',
@@ -71,9 +100,14 @@ export default {
       required: true,
     },
     items: {
-      /** @type {{ new(): Item[] }} */
+      /** @type {SidebarItem[]} */
       type: Array,
       required: true,
+    },
+    navbarDropdown: {
+      /** @type {NavbarDropdown} */
+      type: Object,
+      default: null,
     },
   },
 }
@@ -88,6 +122,15 @@ export default {
   z-index: 100; /* Behind the navbar */
   padding-top: 3.6rem;
   box-shadow: inset -1px 0 0 rgba(0, 0, 0, .1);
+
+  // only SM
+  @media (min-width: 576px) and (max-width: 768px) {
+    width: 12rem;
+  }
+
+  .sidebar-content {
+    width: 11.25rem;
+  }
 }
 
 .sidebar .nav-link {
@@ -101,6 +144,10 @@ export default {
   text-transform: uppercase;
 }
 
+.navbar {
+  height: 3rem;
+}
+
 .navbar-brand {
   padding-top: .75rem;
   padding-bottom: .75rem;
@@ -109,8 +156,12 @@ export default {
   box-shadow: inset -1px 0 0 rgba(0, 0, 0, .25);
 }
 
-.navbar .navbar-toggler {
-  top: .25rem;
-  right: 1rem;
+.navbar-toggler {
+  border: none;
 }
+
+.navbar-toggler:focus {
+  box-shadow: none;
+}
+
 </style>
